15ccfea3e722b600d80e05a3d2576e507647d83c
Matheus Maica <matheus_maica@sicredi.com.br>
2022-10-13 00:03:02 -0300

    funcoes anonimas

---
+++ funcaoAnonima/script.js
@@ -0,0 +1,20 @@
+
+function somar(a,b){
+    let total = a+b;
+    return (total);
+}
+
+console.log(somar(10,4));
+
+let subtrair = (a,b) =>{
+    let total= a- b;
+    return(total)
+}
+console.log(subtrair(33,3));
+
+let numeros = [1,3,4,5]
+
+
+numeros.map((item) => {
+    console.log(item);
+})
